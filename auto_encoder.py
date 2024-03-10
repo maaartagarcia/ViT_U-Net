6946e678a3dd5b944505c4181589963a4ad0b9c6
Marta García Monreal <al404273@banana.init.uji.es>
2024-03-10 20:53:19 +0100

    v1.7 - Encoder-Decoder fully works and it's adapted to RGB Images LEft: ViT implementation, and adapting the code so that it makes predictions, instead of reconstructing the original imags

--- auto_encoder.py
+++ auto_encoder.py
@@ -2,7 +2,10 @@ import keras
 from keras import layers
 
 import matplotlib.pyplot as plt
+
+# Added
 import pdb
+import cv2
 
 def resUnit(input_layer, i, nbF):
     # Input Layer, number of layer, number of filters to be applied
@@ -16,16 +19,18 @@ def resUnit(input_layer, i, nbF):
     return layers.add([input_layer, x])
 
 num_imgs = 1000
-input_shape = (num_imgs, 256, 256, 3)
+input_shape = (num_imgs, 32, 32, 3)
 nbFilter = 32 # Filter size  
 kernel = (3, 3)
 pool_kernel = (2, 2) # Max Pooling Kernel Size
 # Down sample Kernel to convert Encoder and ViT's output to (16, 16, 32) before Decoder
 # Down sample Kernel to convert Decoder to two class maps
 down_kernel = (1,1) 
-batch_size = outSize = 16
+batch_size = 128
+outSize = 16
 upsampling_factor = (4, 4)
 num_classes = 2
+epochs = 400
 
 # input_img = keras.Input(shape=(28, 28, 1))
 input_img = keras.Input(shape=input_shape[1:] )
@@ -121,7 +126,6 @@ x = layers.Conv2D(16, (3, 3), activation='relu')(x)
 x = layers.UpSampling2D((2, 2))(x)
 decoded = layers.Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
 '''
-pdb.set_trace()
 
 # The model uses UpSampling2D layer instead of Conv2DTranspose because the Decoder doesn't learn parameters in order to upsample the feature maps
 
@@ -136,10 +140,13 @@ x = layers.ReLU()(x)
 # Upsampling to batch size (16, 256, 256, 2)
 x = layers.UpSampling2D(size = upsampling_factor, interpolation = 'bilinear')(x)
 # Added
-x = layers.Conv2D(filters = num_classes, kernel_size = kernel, activation = None, padding='same')(x)
+x = layers.Conv2D(filters = num_classes + 1, kernel_size = kernel, activation = None, padding='same')(x)
 x = layers.BatchNormalization()(x)
-x = layers.ReLU()(x)
+decoded = layers.ReLU()(x)
 
+# ------------------------------------------------------------------------------------------------------------------------------
+
+# SET THE MODEL CONFIGURATIONS
 # ------------------------------------------------------------------------------------------------------------------------------
 
 # This model maps an input to its reconstruction
@@ -151,31 +158,49 @@ autoencoder = keras.Model(input_img, decoded)
 # Show the summary of the model architecture
 autoencoder.summary()
 
-autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
+autoencoder.compile(optimizer='adam', loss='binary_crossentropy', metrics = ['accuracy'] )
 # autoencoder.compile(optimizer='adam', loss='mean_squared_error')
 
-from keras.datasets import mnist
+# ------------------------------------------------------------------------------------------------------------------------------
+
+# DATA PREPARATION
+# ------------------------------------------------------------------------------------------------------------------------------
+
+# from keras.datasets import mnist
+from keras.datasets import cifar10
 import numpy as np
 
-(x_train, _), (x_test, _) = mnist.load_data()
+# (x_train, _), (x_test, _) = mnist.load_data()
+(x_train, _), (x_test, _) = cifar10.load_data()
 
 x_train = x_train.astype('float32') / 255.
 x_test  = x_test.astype('float32')  / 255.
-x_train = np.reshape(x_train, (len(x_train), 28, 28, 1))
-x_test =  np.reshape(x_test,  (len(x_test),  28, 28, 1))
+# x_train = np.reshape(x_train, (len(x_train), 28, 28, 1)) 
+# x_test =  np.reshape(x_test,  (len(x_test),  28, 28, 1))
+
+x_train = np.reshape(x_train, (len(x_train), input_shape[1] , input_shape[2], input_shape[3]))
+x_test =  np.reshape(x_test,  (len(x_test), input_shape[1] , input_shape[2], input_shape[3]))
 
 print(x_train.shape)
 print(x_test.shape)
 
+# ------------------------------------------------------------------------------------------------------------------------------
+
+# TRAINING
+# ------------------------------------------------------------------------------------------------------------------------------
+
 history = autoencoder.fit(x_train, x_train,
-                          epochs=50,
-                          batch_size=128,
+                          epochs=epochs,
+                          batch_size=batch_size,
                           shuffle=True,
                           validation_data=(x_test, x_test))
 
-# Learning curves
-epochs = 50
+# ------------------------------------------------------------------------------------------------------------------------------
 
+# LEARNING CURVES
+# ------------------------------------------------------------------------------------------------------------------------------
+
+'''
 plt.figure(2)
 plt.plot(range(1,epochs+1), history.history['loss'])
 plt.plot(range(1,epochs+1), history.history['val_loss'])
@@ -183,6 +208,7 @@ plt.plot(range(1,epochs+1), history.history['val_loss'])
 plt.xlim(1,epochs)
 plt.ylim(0, 0.25)
 plt.show()
+'''
 
 # Encode and decode some digits
 # Note that we take them from the *test* set
@@ -190,22 +216,37 @@ decoded_imgs = autoencoder.predict(x_test)
 
 # Show some image digits examples and their reconstruction
 
+# ------------------------------------------------------------------------------------------------------------------------------
+
+# EXAMPLES RESULT
+# ------------------------------------------------------------------------------------------------------------------------------
+
 n = 10  # How many digits we will display
-plt.figure(figsize=(20, 4))
+# plt.figure(figsize=(20, 4))
 
 for i in range(n):
+    '''
     # Display original
     ax = plt.subplot(2, n, i + 1)
-    plt.imshow(x_test[i].reshape(28, 28))
-    plt.gray()
+    # plt.imshow(x_test[i].reshape(28, 28))
+    plt.imshow(x_test[i])
+    # plt.gray()
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
 
     # Display reconstruction
     ax = plt.subplot(2, n, i + 1 + n)
-    plt.imshow(decoded_imgs[i].reshape(28, 28))
-    plt.gray()
+    # plt.imshow(decoded_imgs[i].reshape(28, 28))
+    plt.imshow(decoded_imgs[i])
+    # plt.gray()
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
 
-plt.show()
+    '''
+    # Added
+    cv2.imwrite('original_' + str(i) + '.jpg', x_test[i] * 255) 
+    cv2.imwrite('predicted_' + str(i) + '.jpg', decoded_imgs[i] * 255) 
+
+# plt.show()
+
+# ------------------------------------------------------------------------------------------------------------------------------
